676ea9af5a4e27b627253d8844a873720d4114eb
Matthew Berthoud <mwberthoud@wm.edu>
2024-04-15 18:22:07 -0400

    neural network implemented, might be bad

--- wiresharkFormatter.py
+++ wiresharkFormatter.py
@@ -1,12 +1,7 @@
-import pandas as pd 
-import matplotlib.pyplot as plt
-from sklearn.utils import resample 
-from sklearn.model_selection import train_test_split  as tts
-from sklearn.preprocessing import scale 
-from sklearn.svm import SVC 
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import confusion_matrix 
-from sklearn.decomposition import PCA
+import pandas as pd
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 chatGPTcsv = "lakeData/4-8_chatGPT.csv"
@@ -14,9 +9,7 @@ blackboardcsv = "lakeData/4-9_blackboard.csv"
 linkedIn = "lakeData/4-9_linkedin.csv"
 
 
-
-#Format our data
-import pandas as pd
+# Format our data
 chatGPT = pd.read_csv(chatGPTcsv)
 print(len(chatGPT))
 blackboard = pd.read_csv(blackboardcsv)
@@ -36,6 +29,26 @@ for protocol in protocols:
         protocolMap[protocol] = len(protocolMap)
 print(protocolMap)
 completeData["Protocol"] = completeData["Protocol"].map(protocolMap)
-numericData = completeData[["Time", "Protocol", "Length","Website"]]
+numericData = completeData[["Time", "Protocol", "Length", "Website"]]
 print(numericData)
 
+
+# Begin neural network
+data = numericData[["Time", "Protocol", "Length"]]
+labels = numericData["Website"]
+
+split = train_test_split(data, labels, test_size=0.2)
+train_data, test_data, train_labels, test_labels = split
+
+clf = MLPClassifier(solver='lbfgs', 
+                    alpha=1e-5,
+                    hidden_layer_sizes=(6,), 
+                    random_state=1)
+clf.fit(train_data, train_labels) 
+
+predictions_train = clf.predict(train_data)
+predictions_test = clf.predict(test_data)
+train_score = accuracy_score(predictions_train, train_labels)
+print("score on train data: ", train_score)
+test_score = accuracy_score(predictions_test, test_labels)
+print("score on test data: ", test_score)
