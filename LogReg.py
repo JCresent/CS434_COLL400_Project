03d1e2dfc2548dd6df3690798410a9e2426ed160
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-05-04 16:46:24 -0400

    adjusted LogReg cfile to include GraphModels class

--- LogReg.py
+++ LogReg.py
@@ -11,6 +11,8 @@ from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.utils import resample
 from sklearn.model_selection import train_test_split as tts
 
+import GraphModels
+
 
 
 protocolMap = {}
@@ -88,7 +90,29 @@ data = convertWireSharkData(chatGPTcsv,blackboardcsv,linkedIncsv )
 
 
 
+def trainData():
+    data = convertWireSharkData("lakeData/train/chatdata.csv","lakeData/train/blackboardData.csv","lakeData/train/linkedindata.csv",7000 )
+    # print(f"Train data contains {len(data)} packets")
+    X = data.drop(columns="Website").values
+    y = data["Website"].astype("category").values
+    # K_range = np.linspace(10,30).astype('int64')
+    # K_range
+    # internalValiation = []
+    # externalValidation = []
 
+    # for k in K_range:
+
+    #     model = KNeighborsClassifier(n_neighbors=k, weights="distance")
+    #     # we need internalTemp and externalTemp values because you dont want to call the qualifier twice which would waste more time and memory
+    #     internalTemp, externalTemp = qualifier(model,X,y)
+    #     internalValiation.append(internalTemp)
+    #     externalValidation.append(externalTemp)
+    print(f" the K range with the highest accuracy {12}, with an accuracy of: {97}%")
+    highestAccuracyK = 12
+    model = KNeighborsClassifier(n_neighbors=highestAccuracyK, weights="distance")
+    model.fit(X, y)
+    print(model.score(X,y))
+    return model
 
 
 X_df = data.drop(columns="Website").values
@@ -112,3 +136,20 @@ confusionMatrix, accuracy = compare_classes(actualClasses,predictedClasses)
 print(confusionMatrix,accuracy)
 
 
+def testWMData(testDataChatgpt, testDataBlackboard, testDataLinkedIn,model):
+    testData = convertWireSharkData(testDataChatgpt,testDataBlackboard,testDataLinkedIn)
+    actualClasses = testData["Website"]
+    testData = testData.drop("Website", axis = 1)
+    predictedClasses = model.predict(testData)
+
+    graph = GraphModels.Graphs(testData,actualClasses,predictedClasses)
+    
+    
+
+
+    return graph.confusionMatrix("LogReg Confusion Matrix"), graph.scatterPlot("Logistic Regression")
+
+model = trainData()
+test = testWMData("lakeData/test/chatgptTestData.csv", "lakeData/test/blackboardTestData.csv", "lakeData/test/linkedinTestData.csv",model)
+# trainedData = testWMData("lakeData/train/chatdata.csv","lakeData/train/blackboardData.csv","lakeData/train/linkedindata.csv",model)
+plt.show()
